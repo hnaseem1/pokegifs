d78e1c7bb24e7dd99c61e06de865d99e82b0dae1
Hasan Naseem <hn92@live.com>
2018-05-08 18:50:12 -0400

    API working

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -35,21 +35,6 @@ class PokemonController < ApplicationController
       end
     end
 
-
-    # @api_data = []
-    # image = []
-    # @pokemon_data["pokemon"].each do |pokemon|
-    #
-    #   pokemon_hash           =   {}
-    #   pokemon_hash["id"]     =   pokemon["id"]
-    #   pokemon_hash["name"]   =   pokemon["name"]
-    #   pokemon_hash["types"]  =   pokemon["type"]
-    #   pokemon_hash["gif"]    =   HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{pokemon["name"]}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
-    #
-    #   @api_data.push(pokemon_hash)
-    #
-    #   end
-
       respond_to do |format|
 
         format.html {render }
