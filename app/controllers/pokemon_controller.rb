e5c362c353a435d8a56ade90e31f11f0f2b8967f
Hasan Naseem <hn92@live.com>
2018-05-08 17:45:40 -0400

    api works the way it is supposed to

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,25 +1,67 @@
 class PokemonController < ApplicationController
   def show
 
-    @request = params[:id]
-    @pokemon_data = JSON.parse(HTTParty.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'))
+    @pokemon_data = JSON.parse(HTTParty.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').body)["pokemon"]
     api_key = ENV['GIFY_KEY']
-    @gify_data = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{@request}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
-    @api_data = []
-    image = []
-    @pokemon_data["pokemon"].each do |pokemon|
+    # @gify_data = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{@request}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
+    @pokemon_hash = {}
 
-      pokemon_hash           =   {}
-      pokemon_hash["id"]     =   pokemon["id"]
-      pokemon_hash["name"]   =   pokemon["name"]
-      pokemon_hash["types"]  =   pokemon["type"]
-      pokemon_hash["gif"]    =   HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{pokemon["name"]}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
+    if params[:id].to_i > 0
+      # if params is an integer find the correct pokemon
+      @pokemon_data.each do |pokemon|
 
-      @api_data.push(pokemon_hash)
+        if pokemon["id"].to_s == params[:id].to_s
+          @pokemon_hash["id"] = pokemon["id"]
+          @pokemon_hash["name"] = pokemon["name"]
+          @pokemon_hash["type"] = pokemon["type"]
+          @pokemon_hash["url"] = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{pokemon["name"]}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
 
-      
+          end
+
+        end
+
+      else
+      # if params is the name of pokemon the url would be the params
+        @pokemon_data.each do |pokemon|
+
+        if pokemon["name"] == params[:id]
+          @pokemon_hash["id"] = pokemon["id"]
+          @pokemon_hash["name"] = pokemon["name"]
+          @pokemon_hash["type"] = pokemon["type"]
+          @pokemon_hash["url"] = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{pokemon["name"]}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
+
+        end
+      end
+    end
+
+
+    # @api_data = []
+    # image = []
+    # @pokemon_data["pokemon"].each do |pokemon|
+    #
+    #   pokemon_hash           =   {}
+    #   pokemon_hash["id"]     =   pokemon["id"]
+    #   pokemon_hash["name"]   =   pokemon["name"]
+    #   pokemon_hash["types"]  =   pokemon["type"]
+    #   pokemon_hash["gif"]    =   HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{pokemon["name"]}&limit=25&offset=0&rating=G&lang=en")["data"][0]["url"]
+    #
+    #   @api_data.push(pokemon_hash)
+    #
+    #   end
+
+      respond_to do |format|
+
+        format.html {render :show}
+        format.json {render json: {
+            id: @pokemon_hash["id"],
+            name: @pokemon_hash["name"],
+            type: @pokemon_hash["type"],
+            gif: @pokemon_hash["url"]
+
+          }}
 
       end
+
     end
 
 end
